fd750a32ba7feb82d7f01a69469eb7cc76c3910f
Ruben E Acosta <reacosta@grupo-exito.com>
2021-04-22 20:53:23 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from './components/Header'
+import Landing from './components/Landing'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Landing />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App
